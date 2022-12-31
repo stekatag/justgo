36d5a4c8c59baf36cda20d7ed23e1eb9ffa188ba
stekatag <stefangogov@protonmail.com>
2022-12-31 13:55:48 +0200

    Added media queries

--- js/script.js
+++ js/script.js
@@ -37,12 +37,12 @@ yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Making the mobile navigation work
-// const btnNavEl = document.querySelector(".btn-mobile-nav");
-// const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-// btnNavEl.addEventListener("click", function () {
-//   headerEl.classList.toggle("nav-open");
-// });
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////
 // Button scrolling
